9262251cbcc284e1c5bcbb921216c77f7435ff3d
LehengChen <chenlh@pku.edu.cn>
2026-04-21 15:59:42 +0800

    Accept erw-elim-v1-0109
    task_id: erw-elim-v1-0109
    target_path: Mathlib/CategoryTheory/Limits/Connected.lean
    artifact_path: /AI4M/users/lhchen/shouyi/results/candidates/erw-elim-v1-0109

--- Mathlib/CategoryTheory/Limits/Connected.lean
+++ Mathlib/CategoryTheory/Limits/Connected.lean
@@ -226,8 +226,8 @@ lemma prod_preservesConnectedLimits [IsConnected J] (X : C) :
             prod.lift (s.π.app (Classical.arbitrary _) ≫ Limits.prod.fst) (l.lift (forgetCone s))
           fac := fun s j => by
             apply Limits.prod.hom_ext
-            · erw [assoc, limMap_π, comp_id, limit.lift_π]
-              exact (nat_trans_from_is_connected (s.π ≫ γ₁ X) j (Classical.arbitrary _)).symm
+            · simpa [ProdPreservesConnectedLimits.γ₁, assoc, limMap_π, limit.lift_π] using
+                (nat_trans_from_is_connected (s.π ≫ γ₁ X) j (Classical.arbitrary _)).symm
             · simp
           uniq := fun s m L => by
             apply Limits.prod.hom_ext
