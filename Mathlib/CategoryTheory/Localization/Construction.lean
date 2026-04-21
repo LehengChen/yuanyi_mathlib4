f3d414f0822368acea333e9604cf0ce7fc16d346
LehengChen <chenlh@pku.edu.cn>
2026-04-21 16:05:27 +0800

    Accept erw-elim-v1-0127
    task_id: erw-elim-v1-0127
    target_path: Mathlib/CategoryTheory/Localization/Construction.lean
    artifact_path: /AI4M/users/lhchen/shouyi/results/candidates/erw-elim-v1-0127

--- Mathlib/CategoryTheory/Localization/Construction.lean
+++ Mathlib/CategoryTheory/Localization/Construction.lean
@@ -331,8 +331,9 @@ def unitIso : 𝟭 (W.Localization ⥤ D) ≅ functor W D ⋙ inverse W D :=
       refine Functor.ext (fun G => ?_) fun G₁ G₂ τ => ?_
       · apply uniq
         dsimp [Functor]
-        erw [fac]
-        rfl
+        exact
+          (fac (W := W) (G := ((functor W D).obj G).obj)
+            (hG := ((functor W D).obj G).property)).symm
       · apply natTrans_hcomp_injective
         ext X
         simp)
