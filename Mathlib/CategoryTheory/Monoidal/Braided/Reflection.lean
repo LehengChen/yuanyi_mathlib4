2b36c502e2c984855772562f0602f9858b16a2b0
LehengChen <chenlh@pku.edu.cn>
2026-04-22 09:08:09 +0800

    Accept erw-elim-v2-0132
    task_id: erw-elim-v2-0132
    target_path: Mathlib/CategoryTheory/Monoidal/Braided/Reflection.lean
    artifact_path: /AI4M/users/lhchen/shouyi/results/candidates/erw-elim-v2-0132

--- Mathlib/CategoryTheory/Monoidal/Braided/Reflection.lean
+++ Mathlib/CategoryTheory/Monoidal/Braided/Reflection.lean
@@ -119,8 +119,8 @@ theorem isIso_tfae : List.TFAE
     -- by `adjRetraction` above.
     let _ : Reflective R := { L := L, adj := adj }
     have : IsIso adj.toMonad.μ := μ_iso_of_reflective (R := R)
-    erw [← adj.toMonad.isSplitMono_iff_isIso_unit]
-    exact ⟨⟨adjRetraction adj _ _, adjRetraction_is_retraction adj _ _⟩⟩
+    exact (Monad.isSplitMono_iff_isIso_unit adj.toMonad _).1
+      ⟨⟨adjRetraction adj _ _, adjRetraction_is_retraction adj _ _⟩⟩
   tfae_have 1 → 3
   | h, d, d' => by
     rw [isIso_iff_isIso_coyoneda_map]
