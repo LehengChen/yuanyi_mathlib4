514442e4183ad071d220bc48be5c5b460af249b7
LehengChen <chenlh@pku.edu.cn>
2026-04-22 09:08:15 +0800

    Accept erw-elim-v2-0157
    task_id: erw-elim-v2-0157
    target_path: Mathlib/CategoryTheory/Sites/DenseSubsite/Basic.lean
    artifact_path: /AI4M/users/lhchen/shouyi/results/candidates/erw-elim-v2-0157

--- Mathlib/CategoryTheory/Sites/DenseSubsite/Basic.lean
+++ Mathlib/CategoryTheory/Sites/DenseSubsite/Basic.lean
@@ -381,7 +381,8 @@ theorem sheafHom_restrict_eq (α : G.op ⋙ ℱ ⟶ G.op ⋙ ℱ'.obj) :
   ext X
   apply yoneda.map_injective
   ext U
-  erw [yoneda.map_preimage]
+  simp only [sheafHom, whiskerLeft_app, yoneda_obj_obj, yoneda_map_app,
+    Functor.map_preimage]
   symm
   change (show (ℱ'.obj ⋙ coyoneda.obj (op (unop U))).obj (op (G.obj (unop X))) from _) = _
   apply sheaf_eq_amalgamation ℱ' (G.is_cover_of_isCoverDense _ _)
@@ -409,7 +410,7 @@ theorem sheafHom_eq (α : ℱ ⟶ ℱ'.obj) : sheafHom (whiskerLeft G.op α) = �
   ext X
   apply yoneda.map_injective
   ext U
-  erw [yoneda.map_preimage]
+  simp only [sheafHom, yoneda_obj_obj, yoneda_map_app, Functor.map_preimage]
   symm
   change (show (ℱ'.obj ⋙ coyoneda.obj (op (unop U))).obj (op (unop X)) from _) = _
   apply sheaf_eq_amalgamation ℱ' (G.is_cover_of_isCoverDense _ _)
