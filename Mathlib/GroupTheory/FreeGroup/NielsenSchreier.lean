2a21f7776570f841bbd55bc3629838551d4fc9b5
LehengChen <chenlh@pku.edu.cn>
2026-04-22 09:08:27 +0800

    Accept erw-elim-v2-0205
    task_id: erw-elim-v2-0205
    target_path: Mathlib/GroupTheory/FreeGroup/NielsenSchreier.lean
    artifact_path: /AI4M/users/lhchen/shouyi/results/candidates/erw-elim-v2-0205

--- Mathlib/GroupTheory/FreeGroup/NielsenSchreier.lean
+++ Mathlib/GroupTheory/FreeGroup/NielsenSchreier.lean
@@ -236,9 +236,10 @@ lemma endIsFree : IsFreeGroup (End (root' T)) :=
       · suffices ∀ {x y} (q : x ⟶ y), F'.map (loopOfHom T q) = (F'.map q : X) by
           rintro ⟨⟨a, b, e⟩, h⟩
           -- Work around the defeq `X = End (F'.obj (IsFreeGroupoid.SpanningTree.root' T))`
-          erw [Functor.mapEnd_apply]
-          rw [this, hF']
-          exact dif_neg h
+          convert this (q := of e) using 1
+          · symm
+            rw [hF']
+            exact dif_neg h
         intro x y q
         suffices ∀ {a} (p : Path (root T) a), F'.map (homOfPath T p) = 1 by
           simp only [this, treeHom, comp_as_mul, inv_as_inv, loopOfHom, inv_one, mul_one,
