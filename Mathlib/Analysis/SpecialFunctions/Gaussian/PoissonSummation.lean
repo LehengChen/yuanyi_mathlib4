f62abb4a3fedd9e1e2f832a64ec4719e95f54afd
LehengChen <chenlh@pku.edu.cn>
2026-04-22 10:15:21 +0800

    Accept erw-elim-v2-0085
    task_id: erw-elim-v2-0085
    target_path: Mathlib/Analysis/SpecialFunctions/Gaussian/PoissonSummation.lean
    artifact_path: /AI4M/users/lhchen/shouyi/results/candidates/erw-elim-v2-0085

--- Mathlib/Analysis/SpecialFunctions/Gaussian/PoissonSummation.lean
+++ Mathlib/Analysis/SpecialFunctions/Gaussian/PoissonSummation.lean
@@ -69,8 +69,8 @@ theorem tendsto_rpow_abs_mul_exp_neg_mul_sq_cocompact {a : ℝ} (ha : 0 < a) (s 
     Tendsto (fun x : ℝ => |x| ^ s * rexp (-a * x ^ 2)) (cocompact ℝ) (𝓝 0) := by
   conv in rexp _ => rw [← sq_abs]
   rw [cocompact_eq_atBot_atTop, ← comap_abs_atTop]
-  erw [tendsto_comap'_iff (m := fun y => y ^ s * rexp (-a * y ^ 2))
-      (mem_atTop_sets.mpr ⟨0, fun b hb => ⟨b, abs_of_nonneg hb⟩⟩)]
+  apply (tendsto_comap'_iff (m := fun y => y ^ s * rexp (-a * y ^ 2))
+    (mem_atTop_sets.mpr ⟨0, fun b hb => ⟨b, abs_of_nonneg hb⟩⟩)).mpr
   exact
     (rpow_mul_exp_neg_mul_sq_isLittleO_exp_neg ha s).tendsto_zero_of_tendsto
       (tendsto_exp_atBot.comp <| tendsto_id.const_mul_atTop_of_neg (neg_lt_zero.mpr one_half_pos))
