5764d2bdafc0871a19780dd88af75a2bf01df925
LehengChen <chenlh@pku.edu.cn>
2026-04-22 18:27:32 +0800

    Accept docstring-repair-categorytheory-v1-fullscan-20260422-0030
    task_id: docstring-repair-categorytheory-v1-fullscan-20260422-0030
    target_path: Mathlib/CategoryTheory/Abelian/Preradical/Radical.lean
    artifact_path: /AI4M/users/lhchen/shouyi/results/candidates/docstring-repair-categorytheory-v1-fullscan-20260422-0030
    
    (cherry picked from commit 23e162e02847f8c5b73a8a4119c6abf6b08d5eb5)

--- Mathlib/CategoryTheory/Abelian/Preradical/Radical.lean
+++ Mathlib/CategoryTheory/Abelian/Preradical/Radical.lean
@@ -24,7 +24,7 @@ of the vanishing of `Φ.r` on `Φ.quotient`.
 
 ## Main definitions
 
-* `Preradical.IsRadical` :
+* `Preradical.isRadical` :
   The property that a preradical `Φ` is radical, i.e. that `(Φ.colon Φ) ≅ Φ`.
 
 * `Radical C` :
