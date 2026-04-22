ffbc2975c666a686a33a759f5e4323741e8bdf49
LehengChen <chenlh@pku.edu.cn>
2026-04-22 10:13:59 +0800

    Accept erw-elim-v2-0282
    task_id: erw-elim-v2-0282
    target_path: Mathlib/Topology/UniformSpace/Matrix.lean
    artifact_path: /AI4M/users/lhchen/shouyi/results/candidates/erw-elim-v2-0282

--- Mathlib/Topology/UniformSpace/Matrix.lean
+++ Mathlib/Topology/UniformSpace/Matrix.lean
@@ -31,9 +31,11 @@ instance instIsUniformAddGroup [AddGroup 𝕜] [IsUniformAddGroup 𝕜] :
 
 theorem uniformity :
     𝓤 (Matrix m n 𝕜) = ⨅ (i : m) (j : n), (𝓤 𝕜).comap fun a => (a.1 i j, a.2 i j) := by
-  erw [Pi.uniformity]
-  simp_rw [Pi.uniformity, Filter.comap_iInf, Filter.comap_comap]
-  rfl
+  have h : 𝓤 (m → n → 𝕜) = ⨅ (i : m) (j : n), (𝓤 𝕜).comap fun a => (a.1 i j, a.2 i j) := by
+    rw [Pi.uniformity]
+    simp_rw [Pi.uniformity, Filter.comap_iInf, Filter.comap_comap]
+    rfl
+  exact h
 
 theorem uniformContinuous {β : Type*} [UniformSpace β] {f : β → Matrix m n 𝕜} :
     UniformContinuous f ↔ ∀ i j, UniformContinuous fun x => f x i j := by
