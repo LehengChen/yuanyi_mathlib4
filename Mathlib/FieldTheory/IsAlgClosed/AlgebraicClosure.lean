2cdfb91c6c52fa532122be057ba8893567afa754
LehengChen <chenlh@pku.edu.cn>
2026-04-25 10:09:44 +0800

    Accept erw-elim-v3-0190
    task_id: erw-elim-v3-0190
    target_path: Mathlib/FieldTheory/IsAlgClosed/AlgebraicClosure.lean
    artifact_path: /AI4M/users/lhchen/shouyi/results/candidates/erw-elim-v3-0190

--- Mathlib/FieldTheory/IsAlgClosed/AlgebraicClosure.lean
+++ Mathlib/FieldTheory/IsAlgClosed/AlgebraicClosure.lean
@@ -185,9 +185,7 @@ instance isAlgebraic : Algebra.IsAlgebraic k (AlgebraicClosure k) :=
           simp_rw [← eval_map, Monics.map_eq_prod, eval_prod, Polynomial.map_sub, eval_sub]
           apply Finset.prod_eq_zero (Finset.mem_univ fi.2)
           rw [map_C]
-          -- The `erw` is needed here because the `R` in `eval` is `AlgebraicClosure k`,
-          -- but this has been unfolded in the arguments of `eval`.
-          erw [eval_C]
+          rw [@eval_C (MvPolynomial (Vars k) k ⧸ maxIdeal k)]
           simp⟩
 
 instance : IsAlgClosure k (AlgebraicClosure k) := .of_splits fun f hf _ ↦ by
